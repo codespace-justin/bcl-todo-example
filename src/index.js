9b9478260123bc589d92663763558b7d4d73b027
Justin <justinjenecke@gmail.com>
2023-10-04 11:42:06 +0200

    implements add todo functionality, adds markup for contact form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import { createTodo } from "./components/todo/index.js";
+
 const TODO_ITEMS = []
+
+const addBtn = document.querySelector('#add-todo')
+
+addBtn.addEventListener('click', (e) => {
+    
+    e.preventDefault()
+    createTodo(TODO_ITEMS)
+})
